c804e001ebe0f7e0c4b58e247283adf6ee2fcbed
JerrrrryW <1007648542@qq.com>
2023-03-09 22:46:36 +0800

    Optimized bugs and code structure when switching working labels.

--- demoWindow1.py
+++ demoWindow1.py
@@ -18,7 +18,8 @@ class DemoWindow:
         self.toolGroup = QButtonGroup()
         self.initToolBar()
 
-        self.selectedImgNum = 0
+        self.selectedImgNum = 0  # 0,1,2   0 means no img label selected
+        self.selectedToolNum = -1  # -1,0,1,2,3   -1 means default moving mode
         self.imageLabels = [self.ui.imageLabel1, self.ui.imageLabel2]  # to access the label faster
 
         self.initClickedBtnConnection()
@@ -39,45 +40,56 @@ class DemoWindow:
         self.ui.horizontalLayout_6.addWidget(self.ui.imageLabel2)
 
     def initToolBar(self):
+        self.toolGroup.addButton(self.ui.moveBtn, 5)
         self.toolGroup.addButton(self.ui.floodFillBtn, 0)
         self.toolGroup.addButton(self.ui.squareCutBtn, 1)
         self.toolGroup.addButton(self.ui.freeCutBtn, 2)
         self.toolGroup.addButton(self.ui.sensorBtn, 3)
         self.toolGroup.setExclusive(True)
+        self.ui.moveBtn.setChecked(True)  # the default tool
 
     def initClickedBtnConnection(self):
         # image file uploading
         self.ui.selectBtn1.clicked.connect(lambda: self.open(self.ui.imageLabel1))
         self.ui.selectBtn2.clicked.connect(lambda: self.open(self.ui.imageLabel2))
         # highlight the groupbox corresponding to the selected image label
-        global_refresh_result_signal.highlight_selected_box.connect(self.onLabelClicked)
+        global_refresh_result_signal.highlight_selected_box.connect(self.onLabelSwitched)
 
         self.toolGroup.buttonPressed[int].connect(lambda _:  # [int] 指定了信号传递的为触发的按钮id
                                                   self.onToolBtnClicked(_, self.imageLabels[self.selectedImgNum]))
         self.toolGroup.buttonReleased[int].connect(lambda:
                                                    self.onToolBtnReleased(self.imageLabels[self.selectedImgNum]))
 
-    def onLabelClicked(self, index: int):
+    def onLabelSwitched(self, index: int):
+        print(f"Working image label switched to groupbox {index}")
+        self.selectedImgNum = index - 1
         # the corresponding GroupBox to blue and the other groupboxes to grey
-        print(f"groupbox {index} clicked!")
         if index == 1:
+            self.ui.imageLabel1.toolIndex = self.selectedToolNum  # Sync the tool selection
             self.ui.imageLabel1.isSelected = True
             self.ui.imageLabel2.isSelected = False
             self.ui.image1GroupBox.setStyleSheet("QGroupBox {border: 3px solid blue;}")
             self.ui.image2GroupBox.setStyleSheet("")
         elif index == 2:
+            self.ui.imageLabel1.toolIndex = self.selectedToolNum
             self.ui.imageLabel1.isSelected = False
             self.ui.imageLabel2.isSelected = True
             self.ui.image1GroupBox.setStyleSheet("")
             self.ui.image2GroupBox.setStyleSheet("QGroupBox {border: 3px solid blue;}")
 
     def onToolBtnClicked(self, clickedBtnID, selectedImageLb: scalableImageLabel):
-        selectedImageLb.toolIndex = clickedBtnID
+        selectedImageLb.runWhenToolReleased()
+        if clickedBtnID != 5:
+            selectedImageLb.toolIndex = self.selectedToolNum = clickedBtnID
+        else:
+            selectedImageLb.toolIndex = self.selectedToolNum = -1
         selectedImageLb.runWhenToolSelected()
 
     def onToolBtnReleased(self, selectedImageLb: scalableImageLabel):
-        selectedImageLb.toolIndex = -1
-        selectedImageLb.runWhenToolReleased()
+        # selectedImageLb.toolIndex = -1
+        # selectedImageLb.runWhenToolReleased()
+        pass
+
 
     def open(self, imageLabel):
         self.imgName, imgType = QFileDialog.getOpenFileName(imageLabel, "打开图片", self.Path,
