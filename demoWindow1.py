00a7dc67d967e98bf7d3345c235eafc970e1c6f8
JerrrrryW <1007648542@qq.com>
2023-03-23 13:46:55 +0800

    Fixed the issue that scalable image label2 can not be placed properly

--- demoWindow1.py
+++ demoWindow1.py
@@ -60,22 +60,22 @@ class DemoWindow:
         originGroupBox2.setLayout(self.ui.originImgHLayout2)
 
         # Load custom image label
-        page1 = self.ui.processingStackedWidget.widget(0)
-        self.ui.horizontalLayout_5 = QHBoxLayout()
-        page1.setLayout(self.ui.horizontalLayout_5)
+        page1 = self.ui.processingPage1
         self.ui.imageLabel1 = scalableImageLabel(page1)
         self.ui.imageLabel1.setScaledContents(False)
         self.ui.imageLabel1.setAlignment(QtCore.Qt.AlignCenter)
         self.ui.imageLabel1.setObjectName("imageLabel1")
+        self.ui.horizontalLayout_5 = QHBoxLayout()
+        page1.setLayout(self.ui.horizontalLayout_5)
         self.ui.horizontalLayout_5.addWidget(self.ui.imageLabel1)
 
-        page2 = self.ui.processingStackedWidget.widget(1)
-        self.ui.horizontalLayout_6 = QHBoxLayout()
-        page1.setLayout(self.ui.horizontalLayout_6)
+        page2 = self.ui.processingPage2
         self.ui.imageLabel2 = scalableImageLabel(page2)
         self.ui.imageLabel2.setScaledContents(False)
         self.ui.imageLabel2.setAlignment(QtCore.Qt.AlignCenter)
         self.ui.imageLabel2.setObjectName("imageLabel2")
+        self.ui.horizontalLayout_6 = QHBoxLayout()
+        page2.setLayout(self.ui.horizontalLayout_6)
         self.ui.horizontalLayout_6.addWidget(self.ui.imageLabel2)
 
     def initToolBar(self):
@@ -89,8 +89,8 @@ class DemoWindow:
 
     def initClickedBtnConnection(self):
         # image file uploading
-        self.ui.selectBtn1.clicked.connect(lambda: self.open(self.ui.originImage1))
-        self.ui.selectBtn2.clicked.connect(lambda: self.open(self.ui.originImage2))
+        self.ui.selectBtn1.clicked.connect(lambda: self.openImgAndShow(self.ui.originImage1, self.ui.imageLabel1))
+        self.ui.selectBtn2.clicked.connect(lambda: self.openImgAndShow(self.ui.originImage2, self.ui.imageLabel2))
 
         self.toolGroup.buttonPressed[int].connect(lambda _:  # [int] 指定了信号传递的为触发的按钮id
                                                   self.onToolBtnClicked(_, self.imageLabels[self.selectedImgNum]))
@@ -99,46 +99,6 @@ class DemoWindow:
 
         self.ui.originImage1.clicked.connect(lambda: self.onLabelSwitched(1))
         self.ui.originImage2.clicked.connect(lambda: self.onLabelSwitched(2))
-
-    def initTabBar(self):  # Tri-layer attributes tab bar on the right side
-        primaryTabs = ["形状", "墨色", "笔法", "纹理"]
-        secondaryTabs = [["轮廓", "结构"],  # 形状的二级指标
-                         ["色调", "层次"],  # 墨色的二级指标
-                         ["力量", "技巧"],  # 笔法的二级指标
-                         ["材质", "效果"]]  # 纹理的二级指标
-        tertiaryTabs = [[["线宽", "线锋", "线形"],  # 轮廓的三级指标
-                         ["位置", "大小", "比例", "方向"]],  # 结构的三级指标
-                        [["明度", "色温", "饱和度"],  # 色调的三级指标
-                         ["变化度", "过渡度", "对比度"]],  # 层次的三级指标
-                        [["压力", "弹性", "速度"],  # 力量的三级指标
-                         ["熟练度", "灵活度", "变化度"]],  # 技巧的三级指标
-                        [["类型", "质地", "吸墨性"],  # 材质的三级指标
-                         ["光泽度", "透明度", "模糊度"]]]  # 效果的三级指标
-
-        self.ui.secTabStack.removeWidget(self.ui.secPage1)
-        self.ui.terAttrStack.removeWidget(self.ui.terPage1)
-        for i in range(len(primaryTabs)):  # retrieve the tabs
-            self.ui.primaryTabBar.addItem(primaryTabs[i])  # fill primary indicators in listview
-            # create pages dynamically to secondaryTabStack
-            secPage = QtWidgets.QWidget()
-            secPage.setObjectName(f"secondaryTabPage{i}")
-            secLayout = QtWidgets.QHBoxLayout(secPage)
-
-            listview = QtWidgets.QListWidget()
-            for j in range(len(secondaryTabs[i])):
-                listview.addItem(secondaryTabs[i][j])
-                # init terAttrPage in stackedWidget
-                terPage = QtWidgets.QWidget()
-                terPage.setObjectName(f"tertiaryAttrPage{i}-{j}")
-                terLayout = QtWidgets.QVBoxLayout(terPage)
-                terLayout.addWidget(self.createTertiaryAttributesGroupBox(tertiaryTabs[i][j], i, j, 1))
-                terLayout.addWidget(self.createTertiaryAttributesGroupBox(tertiaryTabs[i][j], i, j, 2))
-                self.ui.terAttrStack.addWidget(terPage)
-            secLayout.addWidget(listview)
-            listview.currentRowChanged.connect(self.on_row_changed(i))
-
-            self.ui.secTabStack.addWidget(secPage)
-        self.ui.primaryTabBar.currentRowChanged.connect(self.ui.secTabStack.setCurrentIndex)
 
     def on_row_changed(self, i):
         return lambda row: self.ui.terAttrStack.setCurrentIndex(2 * i + row)
@@ -189,22 +149,62 @@ class DemoWindow:
         # selectedImageLb.runWhenToolReleased()
         pass
 
-    def open(self, imageLabel):
+    def openImgAndShow(self, imageLabel: ClickableLabel, displayLabel: scalableImageLabel):
         self.imgName, imgType = QFileDialog.getOpenFileName(imageLabel, "打开图片", self.Path,
                                                             "*.jpg;;*.png;;All Files(*)")
-        self.jpg = QPixmap(self.imgName)
-        print("origin:", self.jpg.width(), self.jpg.height())
+        jpg = QPixmap(self.imgName)
+        print("origin:", jpg.width(), jpg.height())
         print("label:", imageLabel.width(), imageLabel.height())
 
         # Scale the image to fit within the size of imageLabel
-        scaled_jpg = self.jpg.scaled(imageLabel.size(), QtCore.Qt.KeepAspectRatio)
+        scaled_jpg = jpg.scaled(imageLabel.size(), QtCore.Qt.KeepAspectRatio)
 
         print("scaled:", scaled_jpg.width(), scaled_jpg.height())
 
         if not scaled_jpg.isNull():
             imageLabel.setAlignment(QtCore.Qt.AlignLeft | QtCore.Qt.AlignTop)
             imageLabel.setPixmap(scaled_jpg)
+            displayLabel.setPixmap(jpg)
 
+    # def initTabBar(self):  # Tri-layer attributes tab bar on the right side
+    #     primaryTabs = ["形状", "墨色", "笔法", "纹理"]
+    #     secondaryTabs = [["轮廓", "结构"],  # 形状的二级指标
+    #                      ["色调", "层次"],  # 墨色的二级指标
+    #                      ["力量", "技巧"],  # 笔法的二级指标
+    #                      ["材质", "效果"]]  # 纹理的二级指标
+    #     tertiaryTabs = [[["线宽", "线锋", "线形"],  # 轮廓的三级指标
+    #                      ["位置", "大小", "比例", "方向"]],  # 结构的三级指标
+    #                     [["明度", "色温", "饱和度"],  # 色调的三级指标
+    #                      ["变化度", "过渡度", "对比度"]],  # 层次的三级指标
+    #                     [["压力", "弹性", "速度"],  # 力量的三级指标
+    #                      ["熟练度", "灵活度", "变化度"]],  # 技巧的三级指标
+    #                     [["类型", "质地", "吸墨性"],  # 材质的三级指标
+    #                      ["光泽度", "透明度", "模糊度"]]]  # 效果的三级指标
+    #
+    #     self.ui.secTabStack.removeWidget(self.ui.secPage1)
+    #     self.ui.terAttrStack.removeWidget(self.ui.terPage1)
+    #     for i in range(len(primaryTabs)):  # retrieve the tabs
+    #         self.ui.primaryTabBar.addItem(primaryTabs[i])  # fill primary indicators in listview
+    #         # create pages dynamically to secondaryTabStack
+    #         secPage = QtWidgets.QWidget()
+    #         secPage.setObjectName(f"secondaryTabPage{i}")
+    #         secLayout = QtWidgets.QHBoxLayout(secPage)
+    #
+    #         listview = QtWidgets.QListWidget()
+    #         for j in range(len(secondaryTabs[i])):
+    #             listview.addItem(secondaryTabs[i][j])
+    #             # init terAttrPage in stackedWidget
+    #             terPage = QtWidgets.QWidget()
+    #             terPage.setObjectName(f"tertiaryAttrPage{i}-{j}")
+    #             terLayout = QtWidgets.QVBoxLayout(terPage)
+    #             terLayout.addWidget(self.createTertiaryAttributesGroupBox(tertiaryTabs[i][j], i, j, 1))
+    #             terLayout.addWidget(self.createTertiaryAttributesGroupBox(tertiaryTabs[i][j], i, j, 2))
+    #             self.ui.terAttrStack.addWidget(terPage)
+    #         secLayout.addWidget(listview)
+    #         listview.currentRowChanged.connect(self.on_row_changed(i))
+    #
+    #         self.ui.secTabStack.addWidget(secPage)
+    #     self.ui.primaryTabBar.currentRowChanged.connect(self.ui.secTabStack.setCurrentIndex)
 
 if __name__ == '__main__':
     app = QApplication([])
